3873dc554b25dc51e5306cc6d5e7c785b9e75a92
drbenasmah <drbenasmah2016@gmail.com>
2023-08-26 08:39:11 +0200

    update

---
+++ Assignment-coding/variables.js
@@ -0,0 +1,8 @@
+let country = "Ghana";
+let continent = "Africa";
+let population = 30;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(`It's really going well`);
