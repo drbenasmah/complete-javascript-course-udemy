b57dbb50cc3191be7a2bcc359e27dc14ce63d227
drbenasmah <drbenasmah2016@gmail.com>
2023-09-27 23:48:40 +0200

    Task 1

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -250,21 +250,10 @@ btnSort.addEventListener('click', function (e) {
 
 ///////////////////////////////////////
 ///////////////////////////////////////
-// Math and Rounding
-console.log(Math.sqrt(25));
-console.log(25 ** (1 / 2));
-console.log(8 ** (1 / 3));
-
-console.log(Math.max(5, 18, 23, 11, 2));
-console.log(Math.max(5, 18, '23', 11, 2));
-console.log(Math.max(5, 18, '23px', 11, 2));
-
-console.log(Math.min(5, 18, 23, 11, 2));
-
-console.log(Math.PI * Number.parseFloat('10px') ** 2);
-
-console.log(Math.trunc(Math.random() * 6) + 1);
-
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min) + 1) + min;
-// 0...1 -> 0...(max - min) -> min...max
+// Create a Map
+const fruits = new Map([
+  ['apples', 500],
+  ['bananas', 300],
+  ['oranges', 200],
+]);
+console.log(fruits);
